102d26a154e0e81009f7284f67abdf0ced346882
pavani-pc <145609938+pavani-pc@users.noreply.github.com>
2023-10-05 10:31:41 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,14 +19,19 @@ fruits_to_show=my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 st.dataframe(fruits_to_show) 
 st.header("Fruityvice Fruit Advice!")
-fruit_choice=st.text_input('What fruit would you like information about?','kiwi')
-st.write('The user entered',fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-#st.text(fruityvice_response.json())
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-st.dataframe(fruityvice_normalized)
+try:
+  
+  fruit_choice=st.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    st.error("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    # write your own comment -what does the next line do? 
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    # write your own comment - what does this do?
+    st.dataframe(fruityvice_normalized)
+    
+
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
