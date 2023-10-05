405dc68407b8c4023c03b5f43e2d78e43540fb2f
pavani-pc <145609938+pavani-pc@users.noreply.github.com>
2023-10-05 06:44:51 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import requests
 st.title('My Parents New Healthy Diner')
 st.header('Breakfast Menu')
 st.text(' 🥣 Omega 3 & Blueberry Oatmeal')
@@ -15,3 +16,6 @@ fruits_selected=st.multiselect("Pick some fruits:", list(my_fruit_list.index),['
 fruits_to_show=my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 st.dataframe(fruits_to_show) 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+
